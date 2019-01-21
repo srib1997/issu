8582c53b70c0591ab34e8c6d0591a9a6c9d7332a
comus <comus.leong@gmail.com>
2019-01-21 18:25:33 +0800

    update feed

--- renderer/pages/feed.js
+++ renderer/pages/feed.js
@@ -1,7 +1,37 @@
+/**
+ * 主要
+ *
+ * feed state
+ * - events: {},
+ *   - cacheEvents 最後會 set state.events
+ *   - onConfigChanged 歸0，就是變回開始 state
+ * - scope: null,
+ *   - setScope scope 就會等於 detected.id
+ *     - detected 是 detectScope fn 在 teams 裹面分拆後的野
+ *   - setOnlineState scope 等於 config.currentTeam／currentUser.uid
+ *   - componentDidMount scope 等於 user.uid(string)，執行loadData
+ *   - onConfigChanged
+ *     - navigator.onLine true 時, scope 等於 user.uid(string), 執行loadData
+ *     - navigator.onLine false 時, scope 等於 user.uid 就等於空白 object
+ * - teams: [],
+ *   - setTeams
+ *   - onConfigChanged teams 會等於空的 array
+ *   - cacheEvents
+ * - online: typeof navigator === 'undefined' ? true : navigator.onLine,
+ *   - setOnlineState
+ * - darkMode: false,
+ *   - componentDidMount
+ *   - onThemeChanged
+ * - hasLoaded: false
+ *   - componentDidMount
+ */
+
 // Packages
 import electron from 'electron'
 import { Component } from 'react'
-// Import ms from 'ms'
+import parse from 'date-fns/parse'
+import format from 'date-fns/format'
+import retry from 'async-retry'
 
 // Components
 import Switcher from '../components/feed/switcher'
@@ -9,9 +39,10 @@ import isDarkMode from '../utils/dark-mode'
 import Title from '../components/title'
 import Loading from '../components/feed/loading'
 import NoEvents from '../components/feed/none'
+import EventMessage from '../components/feed/event'
 
 // Styles
-import { feedStyles, pageStyles } from '../styles/pages/feed'
+import { feedStyles, pageStyles, headingStyles } from '../styles/pages/feed'
 
 class Feed extends Component {
   state = {
@@ -26,6 +57,182 @@ class Feed extends Component {
   remote = electron.remote || false
   ipcRenderer = electron.ipcRenderer || false
 
+  // 只有一個參數, firstLoad, 看 app 是不是第一次運行
+  // switcher componentDidMount 的時侯, firstLoad 就會是 true
+  async updateEvents(firstLoad) {
+    console.log('updateEvents', firstLoad)
+    const { teams, scope } = this.state
+
+    // 有任何野滿足唔到佢就不會運行
+    if (!teams || Object.keys(teams).length === 0 || !scope) {
+      return
+    }
+
+    // 拿現在的 team object
+    // Load the focused team first
+    const focusedTeam = teams.find(team => {
+      return team.id === this.state.scope
+    })
+
+    // 拿 team object 在 teams array 裹面排第幾個
+    const focusedIndex = teams.indexOf(focusedTeam)
+
+    // 將現在的 team 讀取 events
+    // It's important that this is being `await`ed
+    try {
+      await this.cacheEvents(focusedTeam.id)
+    } catch (error) {
+      console.log(error)
+    }
+
+    // 如果是第一次運行就到這為止，如果不是第一次就接下去
+    if (!firstLoad) {
+      return
+    }
+
+    // Loop 一 loop teams
+    // Update the feed of events for each team
+    for (const team of teams) {
+      const index = teams.indexOf(team)
+
+      // Don't load the focused team, because we updated
+      // that one already above. We need to test for `undefined` here
+      // because checking if falsy is not ok since the value might
+      // be `0` (beginning of `teams` array)
+      if (focusedIndex !== undefined && index === focusedIndex) {
+        continue
+      }
+
+      // 每條 team 都產生 event
+      // It's important that this is being `await`ed
+      try {
+        await this.cacheEvents(team.id)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  async loadEvents(team) {
+    console.log('loadEvents', team)
+    const events = [
+      {
+        id: '111',
+        created: new Date(),
+        message: 'aaa'
+      },
+      {
+        id: '222',
+        created: new Date(),
+        message: 'bbb'
+      },
+      {
+        id: '3',
+        created: new Date(),
+        message: 'aaa'
+      },
+      {
+        id: '4',
+        created: new Date(),
+        message: 'bbb'
+      },
+      {
+        id: '5',
+        created: new Date(),
+        message: 'aaa'
+      },
+      {
+        id: '6',
+        created: new Date(),
+        message: 'bbb'
+      },
+      {
+        id: '3',
+        created: new Date(),
+        message: 'aaa'
+      },
+      {
+        id: '4',
+        created: new Date(),
+        message: 'bbb'
+      },
+      {
+        id: '5',
+        created: new Date(),
+        message: 'aaa'
+      },
+      {
+        id: '6',
+        created: new Date(),
+        message: 'bbb'
+      },
+      {
+        id: '3',
+        created: new Date(),
+        message: 'aaa'
+      },
+      {
+        id: '4',
+        created: new Date(),
+        message: 'bbb'
+      },
+      {
+        id: '5',
+        created: new Date(),
+        message: 'aaa'
+      },
+      {
+        id: '6',
+        created: new Date(),
+        message: 'bbb'
+      },
+      {
+        id: '3',
+        created: new Date(),
+        message: 'aaa'
+      },
+      {
+        id: '4',
+        created: new Date(),
+        message: 'bbb'
+      },
+      {
+        id: '5',
+        created: new Date(),
+        message: 'aaa'
+      },
+      {
+        id: '6',
+        created: new Date(),
+        message: 'bbb'
+      }
+    ]
+
+    return events
+  }
+
+  // 由 team.id 產生 team 所有 event
+  async cacheEvents(scope) {
+    console.log('cacheEvents', scope)
+    /// 由 this.state 拿取各個 state 的值
+    const { teams } = this.state
+
+    /// scope: 想 cache 的 team Id
+    /// activeScope: 現在選中 team 的 team id
+
+    // relatedCache = 透過 scope 由 teams 中找到 team object
+    const relatedCache = teams.find(item => item.id === scope)
+
+    // 載入嗰個 team 的所有 event
+    const result = await this.loadEvents(relatedCache)
+    const events = Object.assign({}, this.state.events)
+    // 將得到的 event 儲存到 events[team.id]
+    events[scope] = result
+
+    /// set state 存放到 state 到
+    this.setState({ events })
+  }
+
   // 為 feed window 增加 listener
   // 當收到  信息的時侯，就執行 onThemeChanged
   // 依個 fn 係在 componentdidmount 裹執行
@@ -39,13 +246,13 @@ class Feed extends Component {
 
   // 將 scrollingSection 的捲軸捲到最頂頂
   // 在 hidewindow 時執行
-  // clearScroll = () => {
-  //   if (!this.scrollingSection) {
-  //     return
-  //   }
+  clearScroll = () => {
+    if (!this.scrollingSection) {
+      return
+    }
 
-  //   this.scrollingSection.scrollTop = 0
-  // }
+    this.scrollingSection.scrollTop = 0
+  }
 
   lineStates = ['online', 'offline']
 
@@ -94,7 +301,7 @@ class Feed extends Component {
     }
 
     this.setState({
-      scope: 'aaa',
+      scope: 'IZSNu3Ty',
       darkMode: isDarkMode(this.remote),
       hasLoaded: true
     })
@@ -150,13 +357,24 @@ class Feed extends Component {
 
     this.setState({
       online,
-      scope: config.currentTeam ? config.currentTeam : 'aaa'
+      scope: config.currentTeam ? config.currentTeam : 'IZSNu3Ty'
     })
   }
 
   // 何時執行: 由 <Switcher/> 去執行, 當點算不同的 team 時會執行, 睇自己個版還是其他 team 的版
   setScope = scope => {
-    this.setState({ scope })
+    console.log('setScope', scope)
+    this.clearScroll()
+
+    // Update the scope
+    this.setState({ scope }, () => {
+      if (this.state.teams.length === 0) {
+        return
+      }
+
+      // And then pull events for it
+      this.cacheEvents(scope)
+    })
   }
 
   detectScope(property, value) {
@@ -166,6 +384,7 @@ class Feed extends Component {
   // 何時執行: 由於 feed 預設不會知道 teams 是什麼
   // 需要由 switcher 去執行並告訴 feed 有幾多個 teams
   setTeams = async (teams, firstLoad) => {
+    console.log('setTeams', teams, firstLoad)
     if (!teams) {
       // If the teams didn't change, only the events
       // should be updated.
@@ -174,10 +393,21 @@ class Feed extends Component {
       return
     }
 
-    this.setState({ teams })
+    return new Promise(resolve =>
+      this.setState({ teams }, async () => {
+        await retry(() => this.updateEvents(firstLoad), {
+          retries: 5,
+          factor: 2,
+          maxTimeout: 5000
+        })
+
+        resolve()
+      })
+    )
   }
 
-  renderEvents(/* team */) {
+  renderEvents(team) {
+    console.log('renderEvents')
     const { scope, events, online, darkMode } = this.state
 
     if (!online) {
@@ -194,45 +424,44 @@ class Feed extends Component {
       return <NoEvents darkBg={this.state.darkMode} />
     }
 
-    // // const months = {}
+    const months = {}
 
-    // for (const message of filteredEvents) {
-    //   const created = parse(message.created)
-    //   const month = format(created, 'MMMM YYYY')
+    for (const message of scopedEvents) {
+      const created = parse(message.created)
+      const month = format(created, 'MMMM YYYY')
 
-    //   if (!months[month]) {
-    //     months[month] = []
-    //   }
+      if (!months[month]) {
+        months[month] = []
+      }
 
-    //   months[month].push(message)
-    // }
+      months[month].push(message)
+    }
 
-    // const eventList = month => {
-    //   return months[month].map(content => {
-    //     const args = {
-    //       content,
-    //       currentUser: this.state.currentUser,
-    //       team,
-    //       setScopeWithSlug: this.setScopeWithSlug,
-    //       message: content.message,
-    //       group,
-    //       darkBg: this.state.darkMode
-    //     }
+    console.log('months', months)
 
-    //     return <EventMessage {...args} key={content.id} />
-    //   })
-    // }
+    const eventList = month => {
+      return months[month].map(content => {
+        const args = {
+          content,
+          team,
+          message: content.message,
+          darkBg: this.state.darkMode
+        }
 
-    // // We can't just use `month` as the ID for each heading,
-    // // because they would glitch around in that case (as
-    // // the month is the same across scopes)
-    // return Object.keys(months).map(month => [
-    //   <h1 className={this.state.darkMode ? 'dark' : ''} key={scope + month}>
-    //     {month}
-    //     <style jsx>{headingStyles}</style>
-    //   </h1>,
-    //   eventList(month)
-    // ])
+        return <EventMessage {...args} key={content.id} />
+      })
+    }
+
+    // We can't just use `month` as the ID for each heading,
+    // because they would glitch around in that case (as
+    // the month is the same across scopes)
+    return Object.keys(months).map(month => [
+      <h1 className={this.state.darkMode ? 'dark' : ''} key={scope + month}>
+        {month}
+        <style jsx>{headingStyles}</style>
+      </h1>,
+      eventList(month)
+    ])
   }
 
   render() {
